f30419913017fe819e6f49cad6a78e8edf23d3bf
Tutanrhamun <katsuki.hanawa.job@gmail.com>
2017-02-24 16:32:38 +0900

    less

--- app.js
+++ app.js
@@ -9,14 +9,17 @@ var server = http.Server(app);
 server.listen(process.env.PORT || 8080);
 
 var io = socketio(server);
+let sum = 0;
 io.sockets.on('connection', function(socket) {
   socket.on('btn', function(pushed) {
     if(pushed){
-      for(var i = 0; i < 300000; i++){
+      for(var i = 0; i < 10; i++){
         console.log('この数は:' + i + 'であるのだ');
         console.log('Are you Ok?');
         console.log('Lets start!');
         console.log('----------------------------------------');
+        sum += i;
+        if(i === 9) console.log(sum);
       }
     }
   });
